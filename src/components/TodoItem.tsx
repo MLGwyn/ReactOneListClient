adde0c78017449815be2ac7ce13041ab38e3e6f2
Melissa Gwyn <melissagwyn@gmail.com>
2024-05-17 15:54:00 -0400

    simplified code

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -7,22 +7,13 @@ type TodoItemProps = {
 }
 export function TodoItem(props: TodoItemProps) {
   async function toggleCompleteStatus() {
-    if (props.todoItem.complete) {
-      const response = await axios.put(
-        `https://one-list-api.herokuapp.com/items/${props.todoItem.id}?access_token=cohort26`,
-        { item: { complete: false } }
-      )
-      if (response.data === 200) {
-        console.log(response.data)
-      }
-    } else {
-      const response = await axios.put(
-        `https://one-list-api.herokuapp.com/items/${props.todoItem.id}?access_token=cohort26`,
-        { item: { complete: true } }
-      )
-      if (response.status === 200) {
-        console.log(response.data)
-      }
+    const response = await axios.put(
+      `https://one-list-api.herokuapp.com/items/${props.todoItem.id}?access_token=cohort26`,
+      { item: { complete: !props.todoItem.complete } }
+    )
+
+    if (response.status === 200) {
+      console.log(response.data)
     }
   }
 
