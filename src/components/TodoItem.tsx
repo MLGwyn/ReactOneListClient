d626b3029ad4f0912a172f5af0ba61d3f166b8f2
Melissa Gwyn <melissagwyn@gmail.com>
2024-05-17 16:13:27 -0400

    destructure

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -6,24 +6,24 @@ type TodoItemProps = {
   todoItem: TodoItemType
   reloadItems: () => void
 }
-export function TodoItem(props: TodoItemProps) {
+export function TodoItem({ todoItem, reloadItems }: TodoItemProps) {
   async function toggleCompleteStatus() {
     const response = await axios.put(
-      `https://one-list-api.herokuapp.com/items/${props.todoItem.id}?access_token=cohort26`,
-      { item: { complete: !props.todoItem.complete } }
+      `https://one-list-api.herokuapp.com/items/${todoItem.id}?access_token=cohort26`,
+      { item: { complete: !todoItem.complete } }
     )
 
     if (response.status === 200) {
-      props.reloadItems()
+      reloadItems()
     }
   }
 
   return (
     <li
-      className={props.todoItem.complete ? 'completed' : undefined}
+      className={todoItem.complete ? 'completed' : undefined}
       onClick={toggleCompleteStatus}
     >
-      {props.todoItem.text}
+      {todoItem.text}
     </li>
   )
 }
