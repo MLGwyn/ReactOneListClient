f69bec53bf0705bfc8c3e408853446443092d4dc
Melissa Gwyn <melissagwyn@gmail.com>
2024-06-18 12:12:40 -0400

    refactor "list" into its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import logo from './images/sdg-logo.svg'
-import axios from 'axios'
-import { TodoItem } from './components/TodoItem'
+import { ToDoList } from './ToDoList'
 
 export type TodoItemType = {
   id: number
@@ -12,65 +11,13 @@ export type TodoItemType = {
 }
 
 export function App() {
-  const [todoItems, setTodoItems] = useState<TodoItemType[]>([])
-  const [newTodoText, setNewTodoText] = useState('')
-
-  function loadAllTheItems() {
-    async function loadItems() {
-      const response = await axios.get(
-        'https://one-list-api.herokuapp.com/items?access_token=cohort26'
-      )
-      if (response.status === 200) {
-        setTodoItems(response.data)
-      }
-    }
-    loadItems()
-  }
-
-  useEffect(loadAllTheItems, [])
-
-  async function handleCreateNewTodoItem() {
-    const response = await axios.post(
-      'https://one-list-api.herokuapp.com/items?access_token=cohort26',
-      { item: { text: newTodoText } }
-    )
-    if (response.status === 201) {
-      loadAllTheItems()
-      setNewTodoText('')
-    }
-  }
   return (
     <div className="app">
       <header>
         <h1>One List</h1>
       </header>
       <main>
-        <ul>
-          {todoItems.map(function (todoItem) {
-            return (
-              <TodoItem
-                key={todoItem.id}
-                todoItem={todoItem}
-                reloadItems={loadAllTheItems}
-              />
-            )
-          })}
-        </ul>
-        <form
-          onSubmit={function (event) {
-            event.preventDefault()
-            handleCreateNewTodoItem()
-          }}
-        >
-          <input
-            type="text"
-            placeholder="Whats up?"
-            value={newTodoText}
-            onChange={function (event) {
-              setNewTodoText(event.target.value)
-            }}
-          />
-        </form>
+        <ToDoList />
       </main>
       <footer>
         <p>
---
+++ src/ToDoList.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { TodoItem } from './components/TodoItem'
+import { TodoItemType } from './App'
+
+export function ToDoList() {
+  const [todoItems, setTodoItems] = useState<TodoItemType[]>([])
+  const [newTodoText, setNewTodoText] = useState('')
+
+  function loadAllTheItems() {
+    async function loadItems() {
+      const response = await axios.get(
+        'https://one-list-api.herokuapp.com/items?access_token=cohort26'
+      )
+      if (response.status === 200) {
+        setTodoItems(response.data)
+      }
+    }
+    loadItems()
+  }
+
+  useEffect(loadAllTheItems, [])
+
+  async function handleCreateNewTodoItem() {
+    const response = await axios.post(
+      'https://one-list-api.herokuapp.com/items?access_token=cohort26',
+      { item: { text: newTodoText } }
+    )
+    if (response.status === 201) {
+      loadAllTheItems()
+      setNewTodoText('')
+    }
+  }
+  return (
+    <>
+      <ul>
+        {todoItems.map(function (todoItem) {
+          return (
+            <TodoItem
+              key={todoItem.id}
+              todoItem={todoItem}
+              reloadItems={loadAllTheItems}
+            />
+          )
+        })}
+      </ul>
+      <form
+        onSubmit={function (event) {
+          event.preventDefault()
+          handleCreateNewTodoItem()
+        }}
+      >
+        <input
+          type="text"
+          placeholder="Whats up?"
+          value={newTodoText}
+          onChange={function (event) {
+            setNewTodoText(event.target.value)
+          }}
+        />
+      </form>
+    </>
+  )
+}
