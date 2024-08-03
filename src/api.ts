77b5014325435b82088d261ad466bd89918f31e3
Melissa Gwyn <melissagwyn@gmail.com>
2024-08-03 12:14:28 -0400

    created BASE_URL

--- src/api.ts
+++ src/api.ts
@@ -1,42 +1,37 @@
 import axios from 'axios'
 import { TodoItemType } from './App'
 
+const BASE_URL = 'https:/one-list-api.herokuapp.com/items'
+
 export async function getOneTodo(id: string) {
   const response = await axios.get<TodoItemType>(
-    `https://one-list-api.herokuapp.com/items/${id}?access_token=cohort26`
+    `${BASE_URL}/${id}?access_token=cohort26`
   )
   return response.data
 }
 
 export async function deleteOneTodo(id: string) {
-  const response = await axios.delete(
-    `https://one-list-api.herokuapp.com/items/${id}?access_token=cohort26`
-  )
+  const response = await axios.delete(`${BASE_URL}/${id}?access_token=cohort26`)
   return response
 }
 
 export async function getTodos() {
-  return (
-    await axios.get<TodoItemType[]>(
-      'https://one-list-api.herokuapp.com/items?access_token=cohort26'
-    )
-  ).data
+  return (await axios.get<TodoItemType[]>(`${BASE_URL}?access_token=cohort26`))
+    .data
 }
 
 export async function createNewTodoItem(newTodoText: string) {
-  return await axios.post(
-    'https://one-list-api.herokuapp.com/items?access_token=cohort26',
-    { item: { text: newTodoText } }
-  )
+  return await axios.post(`${BASE_URL}?access_token=cohort26`, {
+    item: { text: newTodoText },
+  })
 }
 
 export async function toggleItemComplete(
   id: number | undefined,
   complete: boolean
 ) {
-  const response = axios.put(
-    `https://one-list-api.herokuapp.com/items/${id}?access_token=cohort26`,
-    { item: { complete: !complete } }
-  )
+  const response = axios.put(`${BASE_URL}/${id}?access_token=cohort26`, {
+    item: { complete: !complete },
+  })
   return response
 }
